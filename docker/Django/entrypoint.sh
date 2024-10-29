04c0607464c0f1deb2437ef58ef5f5624cd00ad3
kumaradji <kumaradji@gmail.com>
2024-10-29 19:52:23 +0300

    styles v.2.3

--- docker/Django/entrypoint.sh
+++ docker/Django/entrypoint.sh
@@ -13,16 +13,8 @@ fi
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-<<<<<<< HEAD
-#python manage.py loaddata main/fixtures/initial_data.json
-#echo "from django.contrib.auth.models import User;
-#User.objects.filter(email='$ADMIN_EMAIL').delete();
-#User.objects.create_superuser('$DJANGO_ADMIN_USER', '$ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | python manage.py shell
-=======
 # python manage.py loaddata main/fixtures/initial_data.json
 # echo "from django.contrib.auth.models import User;
 # User.objects.filter(email='$ADMIN_EMAIL').delete();
 # User.objects.create_superuser('$DJANGO_ADMIN_USER', '$ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | python manage.py shell
->>>>>>> 7eb18c199171b08c6fb13490babed127002e7ba8
-
 exec "$@"
