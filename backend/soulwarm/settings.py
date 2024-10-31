d826579ae0d5635b4f0747a418ad5e06e49b3ea9
kumaradji <kumaradji@gmail.com>
2024-10-31 17:26:49 +0300

    локальная работа со стилями. изменены настройки базы.

--- backend/soulwarm/settings.py
+++ backend/soulwarm/settings.py
@@ -153,14 +153,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'soulwarm.wsgi.application'
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_DATABASE'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
